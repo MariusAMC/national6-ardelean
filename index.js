8480db99cb78e522f24f5936dfd30e1167f307b6
MariusAMC <marius20amc@gmail.com>
2021-04-01 20:55:02 +0300

    final stuff

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("JavaScript - Cookies & LocalStorage");
+
+console.log(document.cookie);
+
+if (!localStorage.getItem("name") || !localStorage.getItem("password")) {
+  window.location = "/login.html";
+}
+
+document.getElementById("logout").addEventListener("click", () => {
+  localStorage.removeItem("name");
+  localStorage.removeItem("password");
+  window.location = "/";
+});
